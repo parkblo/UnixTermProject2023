37d002b054d3eb40fad2ea56667b912b2a9aebe1
이유리 <leeri1211@naver.com>
2023-12-17 04:52:50 +0900

    Update client.c
    프로세스 간 통신 및 IO 작업 마무리
    
    To-do...
    - 코드 리팩터링
    - 시간측정 함수 추가

--- Project/Project/client.c
+++ Project/Project/client.c
@@ -1,65 +1,251 @@
 #include "mytest.h"
-#define BUFSIZE 1024*1024
 
-void sig_handler(int signo) {
-	;
-}
-void child(int p[2], int i, pid_t pid[4]);
-void parent(int p[4][2]);
 int main() {
-	struct sigaction act;
-	char buf1[BUFSIZE], buf2[BUFSIZE], buf3[BUFSIZE], buf4[BUFSIZE];
-	int p[4][2], fd[4][2], cpid[4], i, j, p2[2], p3[2], p4[2];
-	pid_t my_pid, pid, r;
+	int c1s1[2], c2s2[2], c3s3[2], c4s4[2];
+	int p1p2[2][2], p1p3[2][2], p1p4[2][2], p2p3[2][2], p2p4[2][2], p3p4[2][2];
+	int i, j, k, fd, cnt, fd2;
 
-	memset(cpid, 0, sizeof(cpid));
-	act.sa_handler = sig_handler;
-	sigaction(SIGUSR1, &act, NULL);
+	int *rData = malloc(sizeof(int) * 1024 * 1024);
+	int(*rcvData)[256] = malloc(sizeof(int[4][256]));
+	int *ioData = malloc(sizeof(int) * 1024);
+	int *sendData = malloc(sizeof(int) * 1024);
 
+	char fileName[10];
+	char* ioNode[4] = { "1.ionode", "2.ionode", "3.ionode", "4.ionode" };
+
+	pid_t pid;
+
+	//Create Pipe
+	pipe(c1s1);
+	pipe(c2s2);
+	pipe(c3s3);
+	pipe(c4s4);
+	for (i = 0; i < 2; i++) {
+		pipe(p1p2[i]);
+		pipe(p1p3[i]);
+		pipe(p1p4[i]);
+		pipe(p2p3[i]);
+		pipe(p2p4[i]);
+		pipe(p3p4[i]);
+	}
+
+	//Client Process
 	for (i = 0; i < 4; i++) {
-		pipe(p[i]);
+		switch (pid = fork()) {
+		case -1:
+			perror("error on fork");
+
+		case 0:
+			sprintf(fileName, "P%d.dat", (i + 1));
+			fd = open(fileName, O_RDONLY);
+			read(fd, rData, sizeof(int) * 1024 * 256);
+
+			if (i == 0) {
+				close(p1p2[0][0]);
+				close(p1p2[1][1]);
+
+				close(p1p3[0][0]);
+				close(p1p3[1][1]);
+
+				close(p1p4[0][0]);
+				close(p1p4[1][1]);
+
+
+				for (j = 0; j < 1024 * 256; j += 1024) {
+					printf("%d \n", rData[j]);
+					memcpy(rcvData[0], rData + j, 256 * sizeof(int));
+					read(p1p2[1][0], rcvData[1], 256 * sizeof(int));
+					read(p1p3[1][0], rcvData[2], 256 * sizeof(int));
+					read(p1p4[1][0], rcvData[3], 256 * sizeof(int));
+
+					cnt = 0;
+					for (k = 0; k < 256; k++) {
+						sendData[cnt++] = rcvData[0][k];
+						sendData[cnt++] = rcvData[1][k];
+						sendData[cnt++] = rcvData[2][k];
+						sendData[cnt++] = rcvData[3][k];
+					}
+					write(c1s1[1], sendData, 1024 * sizeof(int));
+
+				}
+
+				for (j = 0; j < 1024 * 256; j += 1024) {
+					write(p1p2[0][1], rData + (j + 256), 256 * sizeof(int));
+				}
+
+				for (j = 0; j < 1024 * 256; j += 1024) {
+					write(p1p3[0][1], rData + (j + 512), 256 * sizeof(int));
+				}
+				for (j = 0; j < 1024 * 256; j += 1024) {
+					write(p1p4[0][1], rData + (j + 768), 256 * sizeof(int));
+				}
+			}
+			else if (i == 1) {
+				close(p1p2[1][0]);
+				close(p1p2[0][1]);
+
+				close(p2p3[0][0]);
+				close(p2p3[1][1]);
+
+				close(p2p4[0][0]);
+				close(p2p4[1][1]);
+
+				for (j = 0; j < 1024 * 256; j += 1024) {
+					write(p1p2[1][1], rData + j, 256 * sizeof(int));
+				}
+
+
+				for (j = 0; j < 1024 * 256; j += 1024) {
+					read(p1p2[0][0], rcvData[0], 256 * sizeof(int));
+					memcpy(rcvData[1], rData + (j + 256), 256 * sizeof(int));
+					read(p2p3[1][0], rcvData[2], 256 * sizeof(int));
+					read(p2p4[1][0], rcvData[3], 256 * sizeof(int));
+
+					cnt = 0;
+					for (k = 0; k < 256; k++) {
+						sendData[cnt++] = rcvData[0][k];
+						sendData[cnt++] = rcvData[1][k];
+						sendData[cnt++] = rcvData[2][k];
+						sendData[cnt++] = rcvData[3][k];
+					}
+
+					write(c2s2[1], sendData, 1024 * sizeof(int));
+
+
+				}
+				for (j = 0; j < 1024 * 256; j += 1024) {
+					write(p2p3[0][1], rData + (j + 512), 256 * sizeof(int));
+				}
+				for (j = 0; j < 1024 * 256; j += 1024) {
+					write(p2p4[0][1], rData + (j + 768), 256 * sizeof(int));
+				}
+			}
+			else if (i == 2) {
+				close(p1p3[1][0]);
+				close(p1p3[0][1]);
+				close(p2p3[1][0]);
+				close(p2p3[0][1]);
+				close(p3p4[0][0]);
+				close(p3p4[1][1]);
+
+				for (j = 0; j < 1024 * 256; j += 1024) {
+					write(p1p3[1][1], rData + j, 256 * sizeof(int));
+
+				}
+				for (j = 0; j < 1024 * 256; j += 1024) {
+					write(p2p3[1][1], rData + (j + 256), 256 * sizeof(int));
+				}
+				for (j = 0; j < 1024 * 256; j += 1024) {
+					read(p1p3[0][0], rcvData[0], 256 * sizeof(int));
+					read(p2p3[0][0], rcvData[1], 256 * sizeof(int));
+					memcpy(rcvData[2], rData + (j + 512), 256 * sizeof(int));
+					read(p3p4[1][0], rcvData[3], 256 * sizeof(int));
+
+					cnt = 0;
+					for (k = 0; k < 256; k++) {
+						sendData[cnt++] = rcvData[0][k];
+						sendData[cnt++] = rcvData[1][k];
+						sendData[cnt++] = rcvData[2][k];
+						sendData[cnt++] = rcvData[3][k];
+					}
+
+					write(c3s3[1], sendData, 1024 * sizeof(int));
+
+				}
+				for (j = 0; j < 1024 * 256; j += 1024) {
+					write(p3p4[0][1], rData + (j + 768), 256 * sizeof(int));
+				}
+			}
+			else {
+				close(p1p4[1][0]);
+				close(p1p4[0][1]);
+				close(p2p4[1][0]);
+				close(p2p4[0][1]);
+				close(p3p4[1][0]);
+				close(p3p4[0][1]);
+
+				for (j = 0; j < 1024 * 256; j += 1024) {
+					write(p1p4[1][1], rData + j, 256 * sizeof(int));
+				}
+				for (j = 0; j < 1024 * 256; j += 1024) {
+					write(p2p4[1][1], rData + (j + 256), 256 * sizeof(int));
+				}
+				for (j = 0; j < 1024 * 256; j += 1024) {
+					write(p3p4[1][1], rData + (j + 512), 256 * sizeof(int));
+				}
+				for (j = 0; j < 1024 * 256; j += 1024) {
+					read(p1p4[0][0], rcvData[0], 256 * sizeof(int));
+					read(p2p4[0][0], rcvData[1], 256 * sizeof(int));
+					read(p3p4[0][0], rcvData[2], 256 * sizeof(int));
+					memcpy(rcvData[3], rData + (j + 768), 256 * sizeof(int));
+
+					cnt = 0;
+					for (k = 0; k < 256; k++) {
+						sendData[cnt++] = rcvData[0][k];
+						sendData[cnt++] = rcvData[1][k];
+						sendData[cnt++] = rcvData[2][k];
+						sendData[cnt++] = rcvData[3][k];
+					}
+
+					write(c4s4[1], sendData, 1024 * sizeof(int));
+
+				}
+			}
+
+			exit(1);
+
+		default:
+			break;
+		}
+	}
+
+
+	//Server Process
+	for (i = 0; i < 4; i++) {
 		switch (pid = fork()) {
 		case -1:
 			perror("error on fork");
 		case 0:
+			fd2 = open(ioNode[i], O_RDWR | O_CREAT | O_TRUNC | O_APPEND, 0644);
 
-		default:
+			if (i == 0) {
+				close(c1s1[1]);
 
-		}
-	}
-
-	parent(p);
-}
-
-
-void parent(int p[4][2]) {
-	char buf[BUFSIZE];
-	int io_1, io_2, io_3, io_4, i, j;
-	fd_set set;
-
-	io_1 = open("io_node_1", O_RDWR | O_CREAT | O_TRUNC, 0644);
-	io_2 = open("io_node_2", O_RDWR | O_CREAT | O_TRUNC, 0644);
-	io_3 = open("io_node_3", O_RDWR | O_CREAT | O_TRUNC, 0644);
-	io_4 = open("io_node_4", O_RDWR | O_CREAT | O_TRUNC, 0644);
-
-	FD_ZERO(&set);
-	for (i = 0; i < 4; i++) {
-		close(p[i][1]);
-		FD_SET(p[i][0], &set);
-	}
-
-	while (select(p[3][0] + 1, &set, NULL, NULL, NULL) > 0) {
-		for (i = 0; i < 4; i++) {
-			if (FD_ISSET(p[i][0], &set)) {
-				if (read(p[i][0], &buf, BUFSIZE) > 0) {
-					if (i == 0) write(io_1, &buf, BUFSIZE);
-					else if (i == 1) write(io_2, &buf, BUFSIZE);
-					else if (i == 2) write(io_3, &buf, BUFSIZE);
-					else write(io_4, &buf, BUFSIZE);
+				for (j = 0; j < 256; j++) {
+					read(c1s1[0], ioData, 1024 * sizeof(int));
+					write(fd2, ioData, 1024 * sizeof(int));
 				}
 			}
+			if (i == 1) {
+				close(c2s2[1]);
+
+				for (j = 0; j < 256; j++) {
+					read(c2s2[0], ioData, 1024 * sizeof(int));
+					write(fd2, ioData, 1024 * sizeof(int));
+				}
+			}
+			if (i == 2) {
+				close(c3s3[1]);
+
+				for (j = 0; j < 256; j++) {
+					read(c3s3[0], ioData, 1024 * sizeof(int));
+					write(fd2, ioData, 1024 * sizeof(int));
+				}
+			}
+			if (i == 3) {
+				close(c4s4[1]);
+
+				for (j = 0; j < 256; j++) {
+					read(c4s4[0], ioData, 1024 * sizeof(int));
+					write(fd2, ioData, 1024 * sizeof(int));
+				}
+			}
+			exit(1);
+
+		default:
+			break;
 		}
-		if (waitpid(-1, NULL, WNOHANG) == -1) return;
 	}
 
+	waitpid(-1, NULL, 0);
 }
